6997bffdd3318d9186f79cebc44c1cc047cac8e4
Tomasz Bugała <tomaszb@mobilefuse.com>
2023-10-04 09:49:31 +0200

    Add app test module and library module

---
+++ app/src/main/java/com/mobilefuse/iabgpp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.mobilefuse.iabgpp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.iab.gpp.encoder.GppModel
+
+class MainActivity : AppCompatActivity() {
+
+    private val testGppConsentString = "DBACNYA~CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA~1YNN"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        simpleTest()
+    }
+
+    private fun simpleTest() {
+        val gppModel = GppModel(testGppConsentString)
+        Log.d("MF", "Decoded gpp string")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IAB GPP"
+include(":app")
+include(":iabgpp-encoder")
